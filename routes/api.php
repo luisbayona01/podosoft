285257634c54f49475e0c397ecde69caa321e36d
lsuisbayona <luibay@elteimpo.com>
2026-07-04 21:56:47 -0500

    test action

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\Api\TenantController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\WhatsAppWebhookController;
 
-
+//http://192.168.2.9:8000/api/agent/message
+// API Endpoints for AI Agent (n8n)
 
 
 Route::post('/agent/message', AgentController::class);
